eec41a05eca3683a0266804d8e923cd92f47a9b0
itsjustkoii <91440824+NgMKhoi@users.noreply.github.com>
2022-07-13 21:47:37 +0700

    Add files via upload

---
+++ public/js/app.js
@@ -0,0 +1,53 @@
+// slider
+$(document).ready(function () {
+  $(".logo-carousel").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: true,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });
+});
+
+let mybutton = document.querySelector(".btn-scroll-up");
+
+// back to top
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+//slider food
+
